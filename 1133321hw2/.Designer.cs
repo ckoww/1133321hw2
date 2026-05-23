020c80be60d67aacbcc5ac813ec04b3d85707da0
ckoww <hanna22095@gmail.com>
2026-05-23 20:50:49 +0800

    加入專案檔案。

---
+++ 1133321hw2/.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace _1133321hw2
+{
+    partial class gaming
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picCell_4_4 = new System.Windows.Forms.PictureBox();
+            this.picCell_4_3 = new System.Windows.Forms.PictureBox();
+            this.picCell_4_2 = new System.Windows.Forms.PictureBox();
+            this.picCell_4_1 = new System.Windows.Forms.PictureBox();
+            this.picCell_3_4 = new System.Windows.Forms.PictureBox();
+            this.picCell_3_3 = new System.Windows.Forms.PictureBox();
+            this.picCell_3_2 = new System.Windows.Forms.PictureBox();
+            this.picCell_3_1 = new System.Windows.Forms.PictureBox();
+            this.picCell_2_4 = new System.Windows.Forms.PictureBox();
+            this.picCell_2_3 = new System.Windows.Forms.PictureBox();
+            this.picCell_2_2 = new System.Windows.Forms.PictureBox();
+            this.picCell_2_1 = new System.Windows.Forms.PictureBox();
+            this.picCell_1_4 = new System.Windows.Forms.PictureBox();
+            this.picCell_1_3 = new System.Windows.Forms.PictureBox();
+            this.picCell_1_2 = new System.Windows.Forms.PictureBox();
+            this.picCell_1_1 = new System.Windows.Forms.PictureBox();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuRestart = new System.Windows.Forms.ToolStripMenuItem();
+            this.tmrPlayTime = new System.Windows.Forms.Timer(this.components);
+            this.tmrDelay = new System.Windows.Forms.Timer(this.components);
+            this.tmrHint = new System.Windows.Forms.Timer(this.components);
+            this.btnHint = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_4_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_4_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_4_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_4_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_3_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_3_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_3_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_3_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_2_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_2_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_2_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_2_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_1_4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_1_3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_1_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_1_1)).BeginInit();
+            this.mnuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picCell_4_4
+            // 
+            this.picCell_4_4.Location = new System.Drawing.Point(353, 461);
+            this.picCell_4_4.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_4_4.Name = "picCell_4_4";
+            this.picCell_4_4.Size = new System.Drawing.Size(115, 145);
+            this.picCell_4_4.TabIndex = 34;
+            this.picCell_4_4.TabStop = false;
+            this.picCell_4_4.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_4_3
+            // 
+            this.picCell_4_3.Location = new System.Drawing.Point(353, 318);
+            this.picCell_4_3.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_4_3.Name = "picCell_4_3";
+            this.picCell_4_3.Size = new System.Drawing.Size(115, 145);
+            this.picCell_4_3.TabIndex = 33;
+            this.picCell_4_3.TabStop = false;
+            this.picCell_4_3.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_4_2
+            // 
+            this.picCell_4_2.Location = new System.Drawing.Point(353, 175);
+            this.picCell_4_2.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_4_2.Name = "picCell_4_2";
+            this.picCell_4_2.Size = new System.Drawing.Size(115, 145);
+            this.picCell_4_2.TabIndex = 32;
+            this.picCell_4_2.TabStop = false;
+            this.picCell_4_2.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_4_1
+            // 
+            this.picCell_4_1.Location = new System.Drawing.Point(353, 31);
+            this.picCell_4_1.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_4_1.Name = "picCell_4_1";
+            this.picCell_4_1.Size = new System.Drawing.Size(115, 145);
+            this.picCell_4_1.TabIndex = 31;
+            this.picCell_4_1.TabStop = false;
+            this.picCell_4_1.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_3_4
+            // 
+            this.picCell_3_4.Location = new System.Drawing.Point(239, 461);
+            this.picCell_3_4.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_3_4.Name = "picCell_3_4";
+            this.picCell_3_4.Size = new System.Drawing.Size(115, 145);
+            this.picCell_3_4.TabIndex = 30;
+            this.picCell_3_4.TabStop = false;
+            this.picCell_3_4.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_3_3
+            // 
+            this.picCell_3_3.Location = new System.Drawing.Point(239, 318);
+            this.picCell_3_3.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_3_3.Name = "picCell_3_3";
+            this.picCell_3_3.Size = new System.Drawing.Size(115, 145);
+            this.picCell_3_3.TabIndex = 29;
+            this.picCell_3_3.TabStop = false;
+            // 
+            // picCell_3_2
+            // 
+            this.picCell_3_2.Location = new System.Drawing.Point(239, 175);
+            this.picCell_3_2.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_3_2.Name = "picCell_3_2";
+            this.picCell_3_2.Size = new System.Drawing.Size(115, 145);
+            this.picCell_3_2.TabIndex = 28;
+            this.picCell_3_2.TabStop = false;
+            this.picCell_3_2.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_3_1
+            // 
+            this.picCell_3_1.Location = new System.Drawing.Point(239, 31);
+            this.picCell_3_1.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_3_1.Name = "picCell_3_1";
+            this.picCell_3_1.Size = new System.Drawing.Size(115, 145);
+            this.picCell_3_1.TabIndex = 27;
+            this.picCell_3_1.TabStop = false;
+            this.picCell_3_1.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_2_4
+            // 
+            this.picCell_2_4.Location = new System.Drawing.Point(123, 461);
+            this.picCell_2_4.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_2_4.Name = "picCell_2_4";
+            this.picCell_2_4.Size = new System.Drawing.Size(115, 145);
+            this.picCell_2_4.TabIndex = 26;
+            this.picCell_2_4.TabStop = false;
+            this.picCell_2_4.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_2_3
+            // 
+            this.picCell_2_3.Location = new System.Drawing.Point(123, 318);
+            this.picCell_2_3.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_2_3.Name = "picCell_2_3";
+            this.picCell_2_3.Size = new System.Drawing.Size(115, 145);
+            this.picCell_2_3.TabIndex = 25;
+            this.picCell_2_3.TabStop = false;
+            this.picCell_2_3.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_2_2
+            // 
+            this.picCell_2_2.Location = new System.Drawing.Point(123, 175);
+            this.picCell_2_2.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_2_2.Name = "picCell_2_2";
+            this.picCell_2_2.Size = new System.Drawing.Size(115, 145);
+            this.picCell_2_2.TabIndex = 24;
+            this.picCell_2_2.TabStop = false;
+            this.picCell_2_2.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_2_1
+            // 
+            this.picCell_2_1.Location = new System.Drawing.Point(123, 31);
+            this.picCell_2_1.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_2_1.Name = "picCell_2_1";
+            this.picCell_2_1.Size = new System.Drawing.Size(115, 145);
+            this.picCell_2_1.TabIndex = 23;
+            this.picCell_2_1.TabStop = false;
+            this.picCell_2_1.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_1_4
+            // 
+            this.picCell_1_4.Location = new System.Drawing.Point(7, 461);
+            this.picCell_1_4.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_1_4.Name = "picCell_1_4";
+            this.picCell_1_4.Size = new System.Drawing.Size(115, 145);
+            this.picCell_1_4.TabIndex = 22;
+            this.picCell_1_4.TabStop = false;
+            this.picCell_1_4.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_1_3
+            // 
+            this.picCell_1_3.Location = new System.Drawing.Point(7, 318);
+            this.picCell_1_3.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_1_3.Name = "picCell_1_3";
+            this.picCell_1_3.Size = new System.Drawing.Size(115, 145);
+            this.picCell_1_3.TabIndex = 21;
+            this.picCell_1_3.TabStop = false;
+            this.picCell_1_3.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_1_2
+            // 
+            this.picCell_1_2.Location = new System.Drawing.Point(7, 175);
+            this.picCell_1_2.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_1_2.Name = "picCell_1_2";
+            this.picCell_1_2.Size = new System.Drawing.Size(115, 145);
+            this.picCell_1_2.TabIndex = 20;
+            this.picCell_1_2.TabStop = false;
+            this.picCell_1_2.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // picCell_1_1
+            // 
+            this.picCell_1_1.Location = new System.Drawing.Point(7, 31);
+            this.picCell_1_1.Margin = new System.Windows.Forms.Padding(4);
+            this.picCell_1_1.Name = "picCell_1_1";
+            this.picCell_1_1.Size = new System.Drawing.Size(115, 145);
+            this.picCell_1_1.TabIndex = 19;
+            this.picCell_1_1.TabStop = false;
+            this.picCell_1_1.Click += new System.EventHandler(this.picCell_1_1_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblTime.Location = new System.Drawing.Point(13, 620);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(64, 20);
+            this.lblTime.TabIndex = 18;
+            this.lblTime.Text = "PLAY!";
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuGame});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(481, 27);
+            this.mnuMain.TabIndex = 35;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // mnuGame
+            // 
+            this.mnuGame.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuRestart});
+            this.mnuGame.Name = "mnuGame";
+            this.mnuGame.Size = new System.Drawing.Size(64, 23);
+            this.mnuGame.Text = "Game";
+            // 
+            // mnuRestart
+            // 
+            this.mnuRestart.Name = "mnuRestart";
+            this.mnuRestart.Size = new System.Drawing.Size(141, 26);
+            this.mnuRestart.Text = "Restart";
+            this.mnuRestart.Click += new System.EventHandler(this.mnuRestart_Click);
+            // 
+            // tmrPlayTime
+            // 
+            this.tmrPlayTime.Enabled = true;
+            this.tmrPlayTime.Interval = 1000;
+            this.tmrPlayTime.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tmrDelay
+            // 
+            this.tmrDelay.Interval = 1000;
+            this.tmrDelay.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // tmrHint
+            // 
+            this.tmrHint.Interval = 1000;
+            this.tmrHint.Tick += new System.EventHandler(this.timerHint_Tick);
+            // 
+            // btnHint
+            // 
+            this.btnHint.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnHint.Location = new System.Drawing.Point(370, 613);
+            this.btnHint.Name = "btnHint";
+            this.btnHint.Size = new System.Drawing.Size(98, 34);
+            this.btnHint.TabIndex = 36;
+            this.btnHint.Text = "提示";
+            this.btnHint.UseVisualStyleBackColor = true;
+            this.btnHint.Click += new System.EventHandler(this.btnHint_Click);
+            // 
+            // gaming
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 653);
+            this.Controls.Add(this.btnHint);
+            this.Controls.Add(this.mnuMain);
+            this.Controls.Add(this.picCell_4_4);
+            this.Controls.Add(this.picCell_4_3);
+            this.Controls.Add(this.picCell_4_2);
+            this.Controls.Add(this.picCell_4_1);
+            this.Controls.Add(this.picCell_3_4);
+            this.Controls.Add(this.picCell_3_3);
+            this.Controls.Add(this.picCell_3_2);
+            this.Controls.Add(this.picCell_3_1);
+            this.Controls.Add(this.picCell_2_4);
+            this.Controls.Add(this.picCell_2_3);
+            this.Controls.Add(this.picCell_2_2);
+            this.Controls.Add(this.picCell_2_1);
+            this.Controls.Add(this.picCell_1_4);
+            this.Controls.Add(this.picCell_1_3);
+            this.Controls.Add(this.picCell_1_2);
+            this.Controls.Add(this.picCell_1_1);
+            this.Controls.Add(this.lblTime);
+            this.MaximizeBox = false;
+            this.Name = "gaming";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "記憶翻牌";
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_4_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_4_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_4_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_4_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_3_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_3_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_3_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_3_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_2_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_2_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_2_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_2_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_1_4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_1_3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_1_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCell_1_1)).EndInit();
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCell_4_4;
+        private System.Windows.Forms.PictureBox picCell_4_3;
+        private System.Windows.Forms.PictureBox picCell_4_2;
+        private System.Windows.Forms.PictureBox picCell_4_1;
+        private System.Windows.Forms.PictureBox picCell_3_4;
+        private System.Windows.Forms.PictureBox picCell_3_3;
+        private System.Windows.Forms.PictureBox picCell_3_2;
+        private System.Windows.Forms.PictureBox picCell_3_1;
+        private System.Windows.Forms.PictureBox picCell_2_4;
+        private System.Windows.Forms.PictureBox picCell_2_3;
+        private System.Windows.Forms.PictureBox picCell_2_2;
+        private System.Windows.Forms.PictureBox picCell_2_1;
+        private System.Windows.Forms.PictureBox picCell_1_4;
+        private System.Windows.Forms.PictureBox picCell_1_3;
+        private System.Windows.Forms.PictureBox picCell_1_2;
+        private System.Windows.Forms.PictureBox picCell_1_1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuGame;
+        private System.Windows.Forms.ToolStripMenuItem mnuRestart;
+        private System.Windows.Forms.Timer tmrPlayTime;
+        private System.Windows.Forms.Timer tmrDelay;
+        private System.Windows.Forms.Timer tmrHint;
+        private System.Windows.Forms.Button btnHint;
+    }
+}
+
---
+++ 1133321hw2/gaming.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1133321hw2
+{
+    public partial class gaming : Form
+    {
+        List<Image> images = new List<Image>();
+        Random rand = new Random();
+
+        PictureBox firstCard = null;
+        PictureBox secondCard = null;
+             
+        int hintCount = 1;        // 偷看的次數
+
+        bool lockClick = false;   // 翻回等待時禁止點擊
+        int timeCount = 0;        // 計時        
+
+        public gaming()
+        {
+            InitializeComponent();
+            LoadImages();
+            RestartGame();
+        }
+        // 載入圖片（不包含背面）
+        private void LoadImages()
+        {
+            images.Add(Properties.Resources._8_1);
+            images.Add(Properties.Resources._8_2);
+            images.Add(Properties.Resources._8_3);
+            images.Add(Properties.Resources._8_4);
+            images.Add(Properties.Resources._8_5);
+            images.Add(Properties.Resources._8_6);
+            images.Add(Properties.Resources._8_7);
+            images.Add(Properties.Resources._8_8);
+        }
+        private void RestartGame()
+        {
+            // 播放重新開始的音效
+            System.Media.SoundPlayer startPlayer = new System.Media.SoundPlayer(Properties.Resources.start);
+            startPlayer.Play();
+            timeCount = 0;
+            lblTime.Text = "PLAY!";
+            //重置偷看次數與按鈕狀態
+            hintCount = 1;
+            btnHint.Enabled = true;
+
+            tmrPlayTime.Start();
+
+            // 卡片池：8 種 × 2
+            List<Image> cards = new List<Image>();
+            foreach (var img in images)
+            {
+                cards.Add(img);
+                cards.Add(img);
+            }
+
+            // 隨機排列
+            cards = cards.OrderBy(x => rand.Next()).ToList();
+
+            int idx = 0;
+            foreach (var pic in this.Controls.OfType<PictureBox>())
+            {
+                // 設定背面圖片
+                pic.Image = Properties.Resources._8_0;
+
+                // Tag 存 Tuple<Image, bool> → Item1 = 卡片, Item2 = 是否翻開
+                pic.Tag = new Tuple<Image, bool>(cards[idx], false);
+                pic.Enabled = true;
+
+                pic.Click -= picCell_1_1_Click;
+                pic.Click += picCell_1_1_Click;
+
+                //加黑框
+                pic.BorderStyle = BorderStyle.FixedSingle;
+
+                idx++;
+            }
+
+            firstCard = null;
+            secondCard = null;
+            lockClick = false;
+        }
+
+        private void mnuRestart_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+
+
+        private void picCell_1_1_Click(object sender, EventArgs e)
+        {
+            if (lockClick) return;
+
+            PictureBox pic = (PictureBox)sender;
+
+            var t = (Tuple<Image, bool>)pic.Tag;
+
+            // 已翻開 → 不動作
+            if (t.Item2) return;
+
+            // 翻牌
+            pic.Image = t.Item1;
+            pic.Tag = new Tuple<Image, bool>(t.Item1, true);
+
+            if (firstCard == null)
+            {
+                firstCard = pic;
+                return;
+            }
+
+            secondCard = pic;
+
+            // 判斷配對
+            var firstImg = ((Tuple<Image, bool>)firstCard.Tag).Item1;
+            var secondImg = ((Tuple<Image, bool>)secondCard.Tag).Item1;
+
+            if (firstImg == secondImg)
+            {
+                // 配對成功
+                firstCard = null;
+                secondCard = null;
+                CheckWin();
+            }
+            else
+            {
+                // 配對失敗 → 翻回
+                lockClick = true;
+                tmrDelay.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timeCount++;
+            lblTime.Text = $"{timeCount} Seconds";
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            tmrDelay.Stop();
+
+            // 翻回第一張
+            var firstTag = (Tuple<Image, bool>)firstCard.Tag;
+            firstCard.Image = Properties.Resources._8_0;
+            firstCard.Tag = new Tuple<Image, bool>(firstTag.Item1, false);
+
+            // 翻回第二張
+            var secondTag = (Tuple<Image, bool>)secondCard.Tag;
+            secondCard.Image = Properties.Resources._8_0;
+            secondCard.Tag = new Tuple<Image, bool>(secondTag.Item1, false);
+
+            firstCard = null;
+            secondCard = null;
+            lockClick = false; // 玩家可以再次點擊
+        }
+
+        // 判斷是否全部配對完成
+        private void CheckWin()
+        {
+            bool allMatched = true;
+            foreach (var pic in this.Controls.OfType<PictureBox>())
+            {
+                var t = (Tuple<Image, bool>)pic.Tag;
+                if (!t.Item2) // 有未翻開的卡片
+                {
+                    allMatched = false;
+                    break;
+                }
+            }
+
+            if (allMatched)
+            {
+                //停止遊戲計時器
+                tmrPlayTime.Stop();
+                // 播放過關音效
+                SoundPlayer winPlayer = new SoundPlayer(Properties.Resources.win); // 替換成你的資源名稱
+                winPlayer.Play();
+
+                //跳出恭喜完成的對話框，並顯示花費時間
+                string message = $"恭喜過關！\n你總共花了 {timeCount} 秒完成挑戰！";
+                string title = "遊戲完成";
+                MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void timerHint_Tick(object sender, EventArgs e)
+        {
+            tmrHint.Stop();
+
+            // 將所有「未配對成功」的牌翻回背面
+            foreach (var pic in this.Controls.OfType<PictureBox>())
+            {
+                var t = (Tuple<Image, bool>)pic.Tag;
+                if (!t.Item2) // 如果狀態是未翻開 (false)
+                {
+                    pic.Image = Properties.Resources._8_0; // 蓋回背面
+                }
+            }
+
+            // 解除鎖定，讓玩家可以繼續玩
+            lockClick = false;
+        }
+
+        private void btnHint_Click(object sender, EventArgs e)
+        {
+            // 如果次數用完、畫面鎖定中、或是已經翻了第一張牌，就不執行
+            if (hintCount <= 0 || lockClick || firstCard != null) return;
+            // 播放提示音效
+            SoundPlayer hintPlayer = new SoundPlayer(Properties.Resources.hint); // 替換成你的資源名稱
+            hintPlayer.Play();
+
+            hintCount--;            
+            btnHint.Enabled = false;
+
+            // 鎖定點擊，避免偷看時玩家亂點卡片造成邏輯錯誤
+            lockClick = true;
+
+            // 將所有「未配對成功」的牌顯示正面
+            foreach (var pic in this.Controls.OfType<PictureBox>())
+            {
+                var t = (Tuple<Image, bool>)pic.Tag;
+                if (!t.Item2) // 如果狀態是未翻開 (false)
+                {
+                    pic.Image = t.Item1; // 顯示正面圖片 (但不改變 Tag 的狀態)
+                }
+            }
+            tmrHint.Start();
+        }
+    }
+}
